e0f56082280823a5682fbeaf76352ca38d52b2bc
pplucao <148817452+pplucao@users.noreply.github.com>
2024-12-09 20:41:02 -0300

    parque.sql

--- parque.sql
+++ parque.sql
@@ -21,19 +21,17 @@ CREATE TABLE atracao (
     capacidade integer not null
 );
 
-CREATE TABLE funcionarios (
-    id serial primary key,
-    nome text not null,
-    cargo text not null,
-    salario numeric(10,3) not null,
-    atracao_id integer,
-    FOREIGN KEY (atracao_id) REFERENCES atracao(id)
-);
-
-
 CREATE TABLE ingressos (
     id serial primary key, 
     visitantes_id integer references visitantes (id), 
     atracao_id integer references atracao (id), 
     data_visita date not null
+);
+
+CREATE TABLE funcionarios (
+    id serial primary key,
+    nome text not null,
+    cargo text not null,
+    salario numeric(10,3) not null,
+    atracao_id integer references atracao (id)
 );

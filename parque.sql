b1662350afceb3295a61e0e3d755b2d78a200ba4
pplucao <148817452+pplucao@users.noreply.github.com>
2024-12-09 20:29:09 -0300

    parque.sql

---
+++ parque.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS parque;
+
+CREATE DATABASE parque;
+
+\c parque;
+
+
+--2)TABELAS
+
+CREATE TABLE visitantes (
+    id serial primary key, 
+    nome text not null, 
+    data_nascimento date not null, 
+    email text  not null
+);
+
+CREATE TABLE atracao (
+    id serial primary key, 
+    nome text not null, 
+    tipo text not null, 
+    capacidade integer not null
+);
+
+CREATE TABLE funcionarios (
+    id serial primary key,
+    nome text not null,
+    cargo text not null,
+    salario numeric(10,3) not null,
+    atracao_id integer,
+    FOREIGN KEY (atracao_id) REFERENCES atracao(id)
+);
+
+
+CREATE TABLE ingressos (
+    id serial primary key, 
+    visitantes_id integer references visitantes (id), 
+    atracao_id integer references atracao (id), 
+    data_visita date not null
+);
